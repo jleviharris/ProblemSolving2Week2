3aa0ac64560c51bd99516209565f8768b3d6a089
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-02 21:02:44 -0500

    Steps writen for problem 1 and 2

--- app.js
+++ app.js
@@ -1,46 +1,60 @@
 "use strict"
 // 1. Happy numbers
+// (1) Create a function that takes in string
+// (2) split string into two strings
+// (3) square both numbers and add together
+// (4) repeat till it breaks down to 1
+
+
+
+
 
 // 2. Prime Numbers
 // (1) I want a function that takes in a string
 // (2) string to be looped through
-// (3) check for prime numbers
+// (3) check for prime numbers and add if theyre prime
 // (4) only numbers 1-100
-/*
-function PrimeNumbers (string){
-    for (let i=0; i<string.length; i++){
 
+let primeNumbersArray = [];
+/*function PrimeNumbers (array){
+    for (let i=0; i<101;){
+        if(parseInt(array[i]) % 2 = 0){
+            i++;}
+            else primeNumbersArray.push(parseInt(array[i]));
+        };console.log(primeNumbersArray);
     }
-}
+};
+PrimeNumbers(primeNumbersArray);
 */
+function PrimeNumbers (array){
+    for (let i=1; i<101; i++){
+        if ( i % 2&&3 != 0) {
+            array.push(i);
+        }
+    }console.log(array);
+};
+PrimeNumbers(primeNumbersArray);
+
+
+
+
+/*
 // 3. Fibonacci
 // (1) Ill need a function that takes in a string
 // (2) loop through the string
 // (3) check if last number and the previous equal the current
-/*let userChoice = prompt('Please enter your starting number');
-let userArray = [];
-function UserChoiceToArray (userChoice){
-    parseInt(userArray.push(userChoice));
-    parseInt(userArray.push(userChoice));
-    parseInt(userArray.push(userChoice + userChoice));
-};
-
-
-*/
 let userChoice;
 let userArray;
 let cur;
 let oneAfter;
 let twoAfter;
 let oneBefore;
-
 function UserChoiceToArray (userChoice){
     userChoice = prompt('Please enter your starting number');
     userArray = [];
     userArray.push(parseInt(userChoice));
     userArray.push(parseInt(userChoice));
-    userArray.push(parseInt(userChoice) + parseInt(userChoice));
-    
+    userArray.push(parseInt(userChoice) + parseInt(userChoice)); 
 };
 function Fibonacci (){ 
     UserChoiceToArray(userChoice);
@@ -54,6 +68,5 @@ for (let i=2; i<100; i++){
     };
     console.log(userArray);
 };
-
-
 Fibonacci();
+*/
